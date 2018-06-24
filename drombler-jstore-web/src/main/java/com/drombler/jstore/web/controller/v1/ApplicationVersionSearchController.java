a00723cfd07d1e9dbc7ab7a661ec2bcc4660b39c
Florian Brunner <florian.brunner@drombler.com>
2018-06-25 01:06:08 +0200

    latest protocol changes

--- drombler-jstore-web/src/main/java/com/drombler/jstore/web/controller/v1/ApplicationVersionSearchController.java
+++ drombler-jstore-web/src/main/java/com/drombler/jstore/web/controller/v1/ApplicationVersionSearchController.java
@@ -1,10 +1,9 @@
 package com.drombler.jstore.web.controller.v1;
 
 import io.swagger.annotations.Api;
-import org.drombler.jstore.protocol.json.ApplicationVersionInfo;
 import org.drombler.jstore.protocol.json.ApplicationVersionSearchRequest;
 import org.drombler.jstore.protocol.json.ApplicationVersionSearchResponse;
-import org.drombler.jstore.protocol.json.JreInfo;
+import org.drombler.jstore.protocol.json.UpgradableApplication;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,36 +23,26 @@ public class ApplicationVersionSearchController {
     @PostMapping
     public ApplicationVersionSearchResponse startApplicationVersionSearch(@RequestBody ApplicationVersionSearchRequest request) {
         ApplicationVersionSearchResponse response = new ApplicationVersionSearchResponse();
-        List<ApplicationVersionInfo> infos = request.getApplicationIds().stream()
-                .map(applicationId -> {
-                    switch (applicationId.getVendorApplicationId()) {
+        List<UpgradableApplication> upgradableApplications = request.getSelectedApplications().stream()
+                .map(selectedApplication -> {
+                    switch (selectedApplication.getApplicationId().getArtifactId()) {
                         case "drombler-jstore-client":
-                            ApplicationVersionInfo info = new ApplicationVersionInfo();
-                            info.setApplicationId(applicationId);
-                            info.setApplicationVersion("0.1-SNAPTHOT");
-                            JreInfo jreInfo = new JreInfo();
-                            jreInfo.setJreVendorId("oracle");
-                            jreInfo.setJavaMajorVersion("8");
-                            info.setJreInfo(jreInfo);
-                            return info;
+                            UpgradableApplication app = new UpgradableApplication();
+                            app.setApplicationId(selectedApplication.getApplicationId());
+                            app.setLatestUpgradableApplicationVersion("0.1-SNAPTHOT");
+                            return app;
                         case "drombler-jstore-client-service":
-                            ApplicationVersionInfo info2 = new ApplicationVersionInfo();
-                            info2.setApplicationId(applicationId);
-                            info2.setApplicationVersion("0.1-SNAPTHOT");
-                            JreInfo jreInfo2 = new JreInfo();
-                            jreInfo2.setJreVendorId("drombler");
-                            jreInfo2.setJavaMajorVersion("9");
-                            jreInfo2.setCustomJREId("drombler-base");
-                            jreInfo2.getModules().add("java.base");
-                            info2.setJreInfo(jreInfo2);
-                            return info2;
+                            UpgradableApplication app2 = new UpgradableApplication();
+                            app2.setApplicationId(selectedApplication.getApplicationId());
+                            app2.setLatestUpgradableApplicationVersion("0.1-SNAPTHOT");
+                            return app2;
                         default:
                             //TODO
                             return null;
                     }
                 })
                 .collect(Collectors.toList());
-        response.setApplicationVersionInfos(infos);
+        response.setUpgradableApplications(upgradableApplications);
         return response;
     }
 }
