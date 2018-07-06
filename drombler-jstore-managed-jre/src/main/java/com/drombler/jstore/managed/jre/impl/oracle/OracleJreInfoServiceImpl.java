f26ea14610d459e050708ac9bcf1634fd6f4a1f4
Florian Brunner <florian.brunner@drombler.com>
2018-07-07 01:49:53 +0200

    fixes and clean-ups

--- drombler-jstore-managed-jre/src/main/java/com/drombler/jstore/managed/jre/impl/oracle/OracleJreInfoServiceImpl.java
+++ drombler-jstore-managed-jre/src/main/java/com/drombler/jstore/managed/jre/impl/oracle/OracleJreInfoServiceImpl.java
@@ -1,19 +1,32 @@
 package com.drombler.jstore.managed.jre.impl.oracle;
 
 import com.drombler.jstore.managed.jre.JreInfoManager;
+import com.drombler.jstore.model.TransactionalService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.drombler.jstore.protocol.json.OracleJreInfo;
 import org.drombler.jstore.protocol.json.SelectedJRE;
 import org.drombler.jstore.protocol.json.SystemInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
-//@TransactionalService
+@TransactionalService
 public class OracleJreInfoServiceImpl implements JreInfoManager {
     private static final String ORACLE_VENDOR_ID = "oracle";
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private OracleJreInfo oracleJreInfo;
+
     @PostConstruct
-    public void init(){
-    // read oracleJREInfo.json file
+    public void init() throws IOException {
+        try (InputStream is = OracleJreInfoServiceImpl.class.getResourceAsStream("oracleJreInfo.json")) {
+            oracleJreInfo = objectMapper.readValue(is, OracleJreInfo.class);
+        }
     }
 
     @Override
--- drombler-jstore-web/src/main/java/com/drombler/jstore/web/controller/v1/JreVersionSearchController.java
+++ drombler-jstore-web/src/main/java/com/drombler/jstore/web/controller/v1/JreVersionSearchController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import org.drombler.jstore.protocol.json.JreVersionSearchRequest;
 import org.drombler.jstore.protocol.json.JreVersionSearchResponse;
 import org.drombler.jstore.protocol.json.UpgradableJRE;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static com.drombler.jstore.web.controller.RestControllerUtils.V1_PATH;
 
@@ -23,10 +24,16 @@ import static com.drombler.jstore.web.controller.RestControllerUtils.V1_PATH;
 @RestController("JreVersionSearchControllerV1")
 @RequestMapping(path = V1_PATH + "/jre-version-search", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class JreVersionSearchController {
+    @Autowired
+    private List<JreInfoManager> jreVersionManagers;
+
+//    @PostConstruct
+//    public void init() {
+//        jreVersionManagers = SpringFactoriesLoader.loadFactories(JreInfoManager.class, JreInfoManager.class.getClassLoader());
+//    }
 
     @PostMapping
     public JreVersionSearchResponse startJreVersionSearch(@RequestBody JreVersionSearchRequest request) {
-        List<JreInfoManager> jreVersionManagers = SpringFactoriesLoader.loadFactories(JreInfoManager.class, null);
         JreVersionSearchResponse response = new JreVersionSearchResponse();
         List<UpgradableJRE> upgradableJREs = new ArrayList<>();
         request.getSelectedJREs().forEach(selectedJRE -> {
